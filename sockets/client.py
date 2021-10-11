28399b833df1cb15582c871883b8b829dab6e182
Soumyajit <roy.valmont@gmail.com>
2021-10-11 17:52:18 +0530

    defined the client & server sockets

--- sockets/client.py
+++ sockets/client.py
@@ -15,6 +15,23 @@ client= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)  # connect to the server at the port 
 
 
+def send_message(msg):
+    
+    message= msg.encode(FORMAT)
+    msg_length = len(message)
+    send_lenght = str(msg_length).encode(FORMAT)
+
+    send_length += b' ' * (HEADER - len(send_lenght))
+    client.send(send_length)
+    client.send(message)
+
+send_message("hello world")
+
+
+
+
+
+
 
 
 
